32286e8db427e1c40e5b9669c1a59df66685a60a
kretika arora <kretikaarora@gmail.com>
2020-09-28 20:12:06 +0530

    Checks for Option

--- Program.cs
+++ Program.cs
@@ -6,10 +6,35 @@ namespace day4_assignment
     {
         static void Main(string[] args)
         {
-            const int START_POSITION = 0;
+            int START_POSITION = 0;
             Random rand = new Random();
-            int dieNumber = rand.Next(0, 7);
+            const int  NO_PLAY= 0;
+            const int LADDER = 1;
+            const int SNAKE = 2;
+          
+                int dieNumber = rand.Next(1, 7);
+
+                if (dieNumber % 3 == LADDER)
+                {
+                    START_POSITION = START_POSITION + dieNumber;
+                }
+                else if (dieNumber % 3 == SNAKE && START_POSITION >= dieNumber)
+                {
+                    START_POSITION = START_POSITION - dieNumber;
+                }
+                else
+                {
+                    START_POSITION = START_POSITION + 0;
+
+                }
+               
+            }
+
+
+
+
+
 
         }
     }
-}
+
